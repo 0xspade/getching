236be91da65026a45be6c8ae5498f910ebde9d71
Spade <18681023+phspade@users.noreply.github.com>
2020-03-25 16:29:46 +0800

    Update main.go

--- main.go
+++ main.go
@@ -36,35 +36,38 @@ func extractTitle(req *http.Request, resp *http.Response, err error) {
 	if err != nil {
 		return
 	}
-	fmt.Printf(req.URL)
+
 	z := html.NewTokenizer(resp.Body)
-
-
 	for {
 		red := color.New(color.FgRed).SprintFunc()
 		yellow := color.New(color.FgYellow).SprintFunc()
 		cyan := color.New(color.FgCyan).SprintFunc()
 
-		bb, _ := ioutil.ReadAll(resp.Body)
-		bs := string(bb)
-
 		tt := z.Next()
 		if tt == html.ErrorToken {
+			bbb, _ := ioutil.ReadAll(resp.Body)
+			bss := string(bbb)
+			fmt.Printf("[ %s ] No Title (%s) [ %s ]\n", status(resp.StatusCode), yellow(req.URL), red(len(bss)))
 			break
 		}
 
 		t := z.Token()
-		if t.Type == html.StartTagToken && t.Data == "title" {
+
+		if t.Type == html.StartTagToken {
+			if t.Data == "title" {
 				if z.Next() == html.TextToken {
+					//fmt.Printf(z.Token().Data)
+					title := strings.TrimSpace(z.Token().Data)
 
-				title := strings.TrimSpace(z.Token().Data)
-
+					bb, _ := ioutil.ReadAll(resp.Body)
+					bs := string(bb)
 					if len([]rune(title)) != 0 && title != "" {
 						fmt.Printf("[ %s ] %s (%s) [ %s ]\n", status(resp.StatusCode), cyan(title), yellow(req.URL), red(len(bs)))
 					} else {
 						fmt.Printf("[ %s ] Blank Title (%s) [ %s ]\n", status(resp.StatusCode), yellow(req.URL), red(len(bs)))
 					}				
 					break
+				}
 			} 
 		}
 	}
